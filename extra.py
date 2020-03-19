2234911c541c625805d5bcc7984f5d3a2fd734c1
Erfan <parsyab1@gmail.com>
2020-03-19 14:36:37 +0330

    amgsg;sjgjsgjs

--- extra.py
+++ extra.py
@@ -95,6 +95,32 @@ def print_progress_bar (iteration, total, prefix = '', suffix = '', decimals = 1
 	if iteration == total:
 		print()
 
+#TODO correct get_input function and CLI interface
+def get_input() :
+	"""inputs :
+	1) path to music play list
+	2) list of singers
+	3) list of sites to check for specefic singers
+	4)
+	arguments :
+	1) -d download files
+	2) -s save this template for next updates
+	3)  
+	"""
+	parser = argparse.ArgumentParser(description="Music Assistant", usage=usage())
+	parser.add_argument("link", help="main page Link")
+	parser.add_argument("-q", "--quality", help="eg: [124, 360, 480, 720, ...]", default='480')
+	'''parser.add_argument("--debug", action="store_true")'''
+	args = parser.parse_args()
+	link = args.link
+	quality = args.quality
+
+	if args.quality :
+		main()
+	else:
+		usage()
+
+
 def usage():
     return """
         Use "" around link to avoid bad things :>
--- main.py
+++ main.py
@@ -28,30 +28,6 @@ def main ():
 		for link in download_musics:
 			file.write(str(link.encode()))
 
-#TODO correct get_input function and CLI interface
-def get_input() :
-	"""inputs :
-	1) path to music play list
-	2) list of singers
-	3) list of sites to check for specefic singers
-	4)
-	arguments :
-	1) -d download files
-	2) -s save this template for next updates
-	3)  
-	"""
-	parser = argparse.ArgumentParser(description="Music Assistant", usage=usage())
-	parser.add_argument("link", help="main page Link")
-	parser.add_argument("-q", "--quality", help="eg: [124, 360, 480, 720, ...]", default='480')
-	'''parser.add_argument("--debug", action="store_true")'''
-	args = parser.parse_args()
-	link = args.link
-	quality = args.quality
-
-	if args.quality :
-		main()
-	else:
-		usage()
 
 if __name__ == "__main__":
 	if len(sys.argv) >= 2:
