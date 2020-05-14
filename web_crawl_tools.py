5e995d201244afa9b74dbbcdcab1bcee6fbd1b5c
Erfan <parsyab1@gmail.com>
2020-05-15 01:34:12 +0430

    type_finde -> _type_finder  unuseable from outside

--- web_crawl_tools.py
+++ web_crawl_tools.py
@@ -16,10 +16,10 @@ def inc_exc_filter (text, includes, excludes):
     checks if all includes are in text and no one from excludes is in text
     '''
     for inc in includes:
-        if not inc in text:
+        if not inc.lower() in text.lower():
             return False
     for exc in excludes:
-        if exc in text:
+        if exc.lower() in text.lower():
             return False
     return True
 
@@ -36,7 +36,8 @@ def soup_maker(url=None, html=None):
 	soup = bs(data, 'html.parser')
 	return (soup)
 
-def type_finder (url: str, file_format: str) -> list:
+def _type_finder (url: str, file_format: str,
+				includes: list =[], excludes: list =[]) -> list:
 	#TODO add filter list for better search : a filter list for words wich should be in link and a filter list to not be in link
 	# e.g : for music file a filter can be music quality if 320 in link it sould be appended
 	#TODO multy file_format search 
@@ -50,23 +51,25 @@ def type_finder (url: str, file_format: str) -> list:
 	base_netloc = urlparse(url).netloc.split('.')[-2:]
 
 	for link in links :
-		if not link : continue
+		if (not link) and (link == url) and (link in found_links) and (not inc_exc_filter(link, includes, excludes)): continue
 		link_type = urlparse(link).path.split('/')[-1]
 		link_netloc = urlparse(link).netloc.split('.')[-2:]
-		if link != url and base_netloc == link_netloc and link_type.endswith(file_format) and not link in found_links:
+		if (link_type.endswith(file_format)) and (base_netloc == link_netloc):
 			found_links.append(link)
 
 	return found_links
 
-def search_for (base_url: str, file_format: str, depth: int = 0, includes: list =[], excludes: list =[], _far: int =0, _debug: bool =False) -> list:
+def search_for (base_url: str, file_format: str, depth: int = 0,
+				includes: list =[], excludes: list =[],
+				_far: int =0, _debug: bool =False) -> list:
 	"""
 	search for a spesefic file_format in all page from base_url to all link in that page until reach the depth
 	@params:
 	base_url    - Required  : page to start searching (Str)
 	file_format - Required  : type of file to search in pages (Str)
 	depth       - Optional  : how much deep should search (default = 0) (Int)
-	includes     - Optional  : list of words should be in result (default = []) (List)
-	excludes     - Optional  : list of words should not be in result (default = []) (List)
+	includes    - Optional  : list of str (utf-8 encoded) words should be in result (default = []) (List)
+	excludes    - Optional  : list of str (utf-8 encoded) words should not be in result (default = []) (List)
 	"""
 	results = list()
 	if _debug : print(f"[*] {'-'*_far} fearching for [{file_format}] in \"{unquote(base_url)}\"")
@@ -74,16 +77,15 @@ def search_for (base_url: str, file_format: str, depth: int = 0, includes: list 
 	#TODO add a max_page param to limitait the number of page search
 	#TODO add include exclude filter
 
-	for i in type_finder(base_url, file_format):
+	for i in _type_finder(base_url, file_format, includes = includes, excludes = excludes):
 		if i and not i in results:
 			results.append(i)
 
 	if depth > 0:
-		childs = type_finder(base_url, '')#matchs links to other pages (not files)
+		childs = _type_finder(base_url, '')#matchs links to other pages (not files)
 		for child in childs:
-			[results.append(i) for i in search_for(child, '.mp3', depth-1, _far+1) if i and not i in res]
-	
-	results = [res for res in results if inc_exc_filter (res, includes, excludes)]
+			child_returns = search_for(child, '.mp3', depth= depth-1, _far= _far+1, includes = includes, excludes = excludes)
+			[results.append(i) for i in child_returns if i and not i in results]
 
 	return results
 
