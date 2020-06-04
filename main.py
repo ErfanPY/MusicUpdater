3b24dc1498d9928ce0bd7596a066af085632ad87
Erfan <parsyab1@gmail.com>
2020-06-04 17:37:35 +0430

    improve outputing

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ import urllib
 from web_crawl_tools import (soup_maker, download_file, search_for)
 from ui_tools import (print_progress_bar, get_input)
 
-def main (players: dict) -> list:
+def main_finder (players: dict) -> list:
 	'''
-	Takes players dict in {player_name: url_to_playlist}  
+	Takes players dict in {player_name: url_to_playlist}  format
 	returns mp3 files from all players pages
 	'''
 	
 	download_musics = []
 	
 	for player_name, player_url in players.items() :
-		print(f'checking for {player_name} musics')
+		print(f"-[*] CHECKING: [{player_name}] MUSICS ")
 		musics = search_for(player_url, '.mp3', depth = 0, excludes=['remix', '320'])
 		for music_url in musics:
 			music_name = urllib.parse.unquote(music_url.split('/')[-1])
@@ -23,7 +23,7 @@ def main (players: dict) -> list:
 			#TODO make a dict with keys = player_name and values =list of music_url
 			if music_name not in os.listdir("downloads") :
 				download_musics.append(music_url)
-				print(f'#ADED: {music_name}')
+				print(f'-[*] FOUND: music_num = {len(download_musics)} / name = {music_name}')
 	
 	return download_musics
 	
@@ -31,6 +31,7 @@ if __name__ == "__main__":
 	players = {}
 	#TODO correct first line of rad (first line have some symbols wich shold be deleted)
 	#TODO add update_method : takes a player name and searches google for playlist
+	#TODO get args to manage output . download links or save them . all/ none/ specified
 	 
 	#Reads players data from file and pu them in players dictionory
 	with open('update_list.json') as f :
@@ -38,20 +39,47 @@ if __name__ == "__main__":
 			players = json.load(f)
 		except json.decoder.JSONDecodeError :
 			players = {}
-	
-	download_links = main(players)
-	
-	print(f'-[*] {len(download_links)} music found ')
 
-	if download_links and input('-[?] Do you want links be saved in output.txt ? (y/n)') in ['y', '']:
+	downloadable_links = main_finder(players)
+	
+	#from this part code customize output with user inputs 
+	if downloadable_links:	
+
+		output_choice = input("""-[?] Do you want links be saved in output.txt ? 
+	-a to save all musics links
+	-n to don't save any music links
+	-s {musics nums with space betwine} to save specefic musics links\n-> """)
+
+		if '-a' in output_choice :
+			output_links = downloadable_links
+		elif '-n' in output_choice :
+			output_links = []
+		elif '-s' in output_choice :
+			output_choiced_nums = output_choice.split(' ')[1:]
+			output_links = [link for num, link in enumerate(downloadable_links) if str(num+1) in output_choiced_nums]
+					
 		with open('output.txt', 'a') as file :
-			for link in download_links:
+			for link in output_links:
 				file.write(str(link.encode()))
-		print('-[*] Links saved in output.txt')
+		print(f'-[*] {len(output_links)} Links saved in output.txt')
 
-	if  download_links and  input('-[?] Do you want to musics be dowloaded ? (y/n)').lower() in ['y', '']:
+		download_choice = input("""-[?] Do you want links be downloaded ? 
+	-a to download all musics
+	-n to don't download any music
+	-d {musics nums with space betwine} to download specefic musics\n-> """)
+
+		
 		if 'downloads' not in os.listdir():
 			os.mkdir('downloads')
+	
+		if '-a' in download_choice :
+			download_links = downloadable_links
+		elif '-n' in download_choice :
+			download_links = []
+		elif '-s' in download_choice :
+			download_choiced_nums = download_choice.split(' ')[1:]
+			download_links = [link for num, link in enumerate(downloadable_links) if str(num+1) in download_choiced_nums]
+		
 		for link in download_links:
 			download_file(link, dir='downloads/', verbose=True)
 			print(f"-[*] {link.split('/')[-1]} downloaded")
